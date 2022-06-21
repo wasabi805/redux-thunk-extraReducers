d72cec6b43e17eff03cb0f026e2d65ce8ab10b7e
Tim Ocampo <ocampo@Tims-MacBook-Pro.local>
2022-06-21 00:13:21 -0700

    made ButtonWrapper component.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import ButtonWrapper from './ButtonWrapper'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <ButtonWrapper render={() => console.log('hi')} />
+    </div>
+  )
 }
 
 export default App
